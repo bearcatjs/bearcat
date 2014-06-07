f024dbf865ce0878489088aff350901bf9f8e291
fantasyni <fantasyni@163.com>
2014-06-07 10:38:49 +0800

    fix factoryBean factoryMethod run with inject bug

--- examples/simple_factory_bean/carFactory.js
+++ examples/simple_factory_bean/carFactory.js
@@ -1,9 +1,12 @@
 var Car = require('./car');
 
-var CarFactory = function() {}
+var CarFactory = function() {
+	this.wheel = null;
+}
 
 CarFactory.prototype.createCar = function() {
 	console.log('CarFactory createCar...');
+	this.wheel.run();
 	return new Car();
 }
 
