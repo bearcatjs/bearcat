c588d5f2d975a3269c0096aa1a28ba2b928d191f
fantasyni <fantasyni@163.com>
2014-06-25 16:56:35 +0800

    add examples run app.js

--- examples/app.js
+++ examples/app.js
@@ -1,18 +1,5 @@
-these are examples of Bearcat framework  
-
-run examples will be like this:  
-
-```
-node app.js
-```
-
-```
-modify example_dir parameter to run different bearcat examples
-```
-
-```
 var bearcat = require('../lib/bearcat');
-var example_dir = 'simple'; // change this to run different examples
+var example_dir = 'simple'; // change this to run your examples
 // var example_dir = 'simple_function_annotation';
 
 var contextPath = require.resolve('./' + example_dir + '/context.json'); // to run simple example
@@ -22,4 +9,3 @@ bearcat.start(function() {
 	var car = bearcat.getBean('car'); // get bean
 	car.run(); // call the method
 });
-```
