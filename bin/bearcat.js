15108b9080482dbcfa79e2686de02afedea3c6a3
fantasyni <fantasyni@163.com>
2014-12-04 08:57:44 +0800

    review browser support

--- bin/bearcat.js
+++ bin/bearcat.js
@@ -1,26 +1,40 @@
 #!/usr/bin/env node
 
+/*!
+ * .______    _______     ___      .______       ______     ___   .__________.
+ * (   _  )  (   ____)   /   \     (   _  )     (      )   /   \  (          )
+ * |  |_)  ) |  |__     /  ^  \    |  |_)  )   |  ,----'  /  ^  \ `---|  |---`
+ * |   _  <  |   __)   /  /_\  \   |      )    |  |      /  /_\  \    |  |
+ * |  |_)  ) |  |____ /  _____  \  |  |)  ----.|  `----./  _____  \   |  |
+ * (______)  (_______/__/     \__\ ( _| `.____) (______)__/     \__\  |__|
+ *
+ * Bearcat command
+ * Copyright(c) 2014 fantasyni <fantasyni@163.com>
+ * MIT Licensed
+ */
+
 var PropertiesLoader = require('../lib/resource/propertiesLoader');
 var ResourceLoader = require('../lib/resource/resourceLoader');
+var Version = require('../package.json').version;
+var Commander = require('commander');
+
 var fs = require('fs');
 
 var resourceLoader = new ResourceLoader();
+var propertiesLoader = new PropertiesLoader();
 
-var cpath = process.cwd() + '/context.json';
+var rootPath = process.cwd();
+var cpath = rootPath + '/context.json';
+var configPath = rootPath + '/config';
+var env = 'prod';
 
 var metas = resourceLoader.load(cpath);
 
-console.log(metas);
-
-// for (var beanId in metas) {
-// 	var meta = metas[beanId];
-// 	var fpath = meta['fpath'];
-// }
-
-var outputFile = 'bb.js';
+var outputFile = 'bearcat-bootstrap.js';
 fs.writeFileSync(outputFile, 'var metas = ' + JSON.stringify(metas) + ';\n');
-fs.appendFileSync(outputFile, 'window.requireCache = {};\n');
-fs.appendFileSync(outputFile, 'window.metas = {};\n');
+fs.appendFileSync(outputFile, 'window.bearcatjs = {};\n');
+fs.appendFileSync(outputFile, 'window.bearcatjs.metas = {};\n');
+fs.appendFileSync(outputFile, 'window.bearcatjs.configData = {};\n');
 
 for (var id in metas) {
 	var meta = metas[id];
@@ -28,7 +42,11 @@ for (var id in metas) {
 	fs.appendFileSync(outputFile, 'var id = \"' + id + '\";\n');
 	fs.appendFileSync(outputFile, 'var meta = metas\[id\];\n');
 	fs.appendFileSync(outputFile, 'var fpath = meta\[\"fpath\"\];\n');
-	fs.appendFileSync(outputFile, 'meta[\"func\"] = require\(\"./' + fpath + '\"\);\n')
-	fs.appendFileSync(outputFile, 'window.requireCache\[fpath\] = meta;\n')
-	fs.appendFileSync(outputFile, 'window.metas\[id\] = meta;\n');
+	fs.appendFileSync(outputFile, 'meta[\"func\"] = require\(\"./' + fpath + '\"\);\n');
+	fs.appendFileSync(outputFile, 'window.bearcatjs.metas\[id\] = meta;\n');
 }
+
+var properties = propertiesLoader.loadProperties(configPath, env);
+
+fs.appendFileSync(outputFile, 'var properties = ' + JSON.stringify(properties) + ';\n');
+fs.appendFileSync(outputFile, 'window.bearcatjs.configData = properties;\n');
