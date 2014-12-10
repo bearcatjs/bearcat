7889bdecc7573cc9ba3e6b8f3ab91d797b3a7240
fantasyni <fantasyni@163.com>
2014-12-10 17:50:57 +0800

    update shim browser-test.html

--- shim/logger.js
+++ shim/logger.js
@@ -1,4 +1,21 @@
+/*!
+ * .______    _______     ___      .______       ______     ___   .__________.
+ * (   _  )  (   ____)   /   \     (   _  )     (      )   /   \  (          )
+ * |  |_)  ) |  |__     /  ^  \    |  |_)  )   |  ,----'  /  ^  \ `---|  |---`
+ * |   _  <  |   __)   /  /_\  \   |      )    |  |      /  /_\  \    |  |
+ * |  |_)  ) |  |____ /  _____  \  |  |)  ----.|  `----./  _____  \   |  |
+ * (______)  (_______/__/     \__\ ( _| `.____) (______)__/     \__\  |__|
+ *
+ * Bearcat shim logger.js
+ * Copyright(c) 2014 fantasyni <fantasyni@163.com>
+ * MIT Licensed
+ */
+
 function getLogger(categoryName) {
+	if (typeof console.log !== 'function') {
+		return console;
+	}
+
 	var args = arguments;
 	var prefix = "";
 	for (var i = 1; i < args.length; i++) {
@@ -17,25 +34,30 @@ function getLogger(categoryName) {
 		pLogger[key] = logger[key];
 	}
 
-	['log', 'debug', 'info', 'warn', 'error', 'trace', 'fatal'].forEach(function(item) {
-		pLogger[item] = function() {
-			var p = "";
-			if (!process.env.RAW_MESSAGE) {
-				if (args.length > 1) {
-					p = "[" + prefix + "] ";
-				}
-				if (args.length && process.env.LOGGER_LINE) {
-					p = getLine() + ": " + p;
-				}
-				// p = colorize(p, colours[item]);
-			}
+	var levels = ['log', 'debug', 'info', 'warn', 'error', 'trace'];
 
-			if (args.length) {
-				arguments[0] = p + arguments[0];
+	for (var i = 0; i < levels.length; i++) {
+		(function(item) {
+			pLogger[item] = function() {
+				var p = "";
+				if (!process.env.RAW_MESSAGE) {
+					if (args.length > 1) {
+						p = "[" + prefix + "] ";
+					}
+					if (args.length && process.env.LOGGER_LINE) {
+						p = getLine() + ": " + p;
+					}
+				}
+
+				if (args.length) {
+					arguments[0] = p + arguments[0];
+				}
+
+				logger[item].apply(logger, arguments);
 			}
-			logger[item].apply(logger, arguments);
-		}
-	});
+		})(levels[i]);
+	}
+
 	return pLogger;
 };
 
